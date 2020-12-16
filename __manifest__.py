e67b5db430b5502c896ac34a15d005179a166dea
kevinfranciscokf1 <43979489+kevinfranciscokf1@users.noreply.github.com>
2020-12-16 17:01:26 -0600

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Event_Registration',
+    'version': '12.0.1.0.0',
+    'author': 'Kevin Lopez',
+    'website': 'https://github.com/kevinfranciscokf1',
+    'category': 'Event',
+    'depends': [
+        'website_event',
+    ],
+    'data': [
+        'views/event_views.xml',
+        'views/website_event_templates.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
